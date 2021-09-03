f60cdfede90cb6e9fe2ec41bf4b009b95d588a3a
Gorokhovik Dmitry <dima_lgtu@mail.ru>
2021-09-03 18:45:07 +0300

    done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <img class="container-background__desktop" :src="backgroundDesktop" alt="">
+    <img class="container-background__phone" :src="backgroundPhone" alt="">
+    <img class="logo" :src="logo" alt="">
+    <a class="light-start" href="/">
+      <img :src="lightStart" alt="">
+    </a>
+    <a class="fitness" href="/">
+      <img :src="fitness" alt="">
+    </a>
+    <a class="question" href="/">
+      <p>Задать вопрос</p>
+      <img :src="telegram" alt="">
+    </a>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import backgroundDesktop from '../assets/background-desktop.svg'
+import backgroundPhone from '../assets/background-phone.svg'
+import fitness from '../assets/fitnes.svg'
+import lightStart from '../assets/light-start.svg'
+import logo from '../assets/logo.svg'
+import telegram from '../assets/telegram.svg'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  data(){
+    return{
+      backgroundDesktop, backgroundPhone, fitness, lightStart, logo, telegram
+    }
   }
 }
 </script>
+
